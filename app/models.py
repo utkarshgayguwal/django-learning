633a5f8596503ac25b1b5571bda088ddb42656cf
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-08-28 19:06:49 +0530

    chore: added model for generalinfo, ran => 'python3 manage.py makemigrations' and 'python3 manage.py migrate'

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class GeneralInfo(models.Model):
+    company_name = models.CharField(max_length=255, default="Company")
+    location = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    open_hours = models.CharField(max_length=100, blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
+    twitter_url = models.URLField(blank=True, null=True)
+    facebook_url = models.URLField(blank=True, null=True)
+    instagram_url = models.URLField(blank=True, null=True)
+    linkedin_url = models.URLField(blank=True, null=True)
+
+
