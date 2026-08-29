c6fe7fb5cc152fe1fdde6b0a0e22738c2072bbfb
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-08-29 07:34:50 +0530

    feat: render testimonials section from Testimonial database records instead of hardcoded HTML

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from app.models import GeneralInfo
 from app.models import Service
+from app.models import Testimonial
 
 def index(request):
     general_info = GeneralInfo.objects.first()
     services = Service.objects.all()
-    context = {'general_info' : general_info, 'services': services}
+    testimonials = Testimonial.objects.all()
+    context = {'general_info' : general_info, 'services': services, 'testimonials': testimonials}
     return render(request, 'index.html', context)
