9175578c6eda6189ec93b5a6cca644d95f6a8f5a
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-08-29 06:31:24 +0530

    feat: render services section from Service database records instead of hardcoded HTML

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from app.models import GeneralInfo
+from app.models import Service
 
 def index(request):
     general_info = GeneralInfo.objects.first()
-    context = {'general_info' : general_info}
+    services = Service.objects.all()
+    context = {'general_info' : general_info, 'services': services}
     return render(request, 'index.html', context)
